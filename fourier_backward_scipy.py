8ad7997c234b69dbb80e7214cf306b31b5ba5a06
samuelhklumpers <samuelhklumpers@gmail.com>
2022-03-04 12:03:26 +0100

    fourier things

---
+++ fourier_backward_scipy.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+import scipy as sp
+import scipy.integrate
+
+import matplotlib.pyplot as plt
+
+# F = float
+
+k = 1 # wavenumber
+lims = (-1, 1)
+
+def gauss(s):
+    def u(x, y):
+        return np.exp(-(x ** 2 + y ** 2) / 2 / s ** 2) / np.sqrt(2 * np.pi) / s
+    return u
+
+def cdblquad(f, *args, **kwargs):
+    def r(y, x):
+        return f(y, x).real
+    def i(y, x):
+        return f(y, x).imag
+
+    rr, re = scipy.integrate.dblquad(r, *args, **kwargs) 
+    ir, ie = scipy.integrate.dblquad(i, *args, **kwargs)
+
+    return rr + 1.j * ir
+
+def R(d): # d : F
+    def f(u): # u : (F, F) -> F
+        def v(x, y): # x, y : F
+            def integrand(eta, xi): # intentionally flipped, see sp.dblquad
+                p = (x - xi) ** 2 + (y - eta) ** 2
+
+                return u(xi, eta) * np.exp(1.j * k / (2 * d) * p)
+
+            m = k / (2 * np.pi * 1.j * d)
+
+            res = cdblquad(integrand, *lims, *lims, epsabs=1e-3)
+
+            return m * res
+
+        return v
+    return f
+
+def image(u, X, Y):
+    U = np.zeros(X.shape, dtype=complex)
+    
+    for i, _ in np.ndenumerate(U):
+        x, y = X[i], Y[i]
+        U[i] = u(x, y)
+
+    return np.absolute(U) ** 2
+
+
+def main():
+    u1 = gauss(0.2)
+
+    xs = np.linspace(-1, 1, num=20)
+    X, Y = np.meshgrid(xs, xs)
+
+    def show_im(im, title):
+        plt.figure()
+        plt.title(title)
+        r = plt.pcolormesh(X, Y, im, shading='auto')
+        plt.colorbar(r)
+        plt.show(block=False)
+
+    R1 = R(0.5)
+    #Q1 = Q(-1 / 2)
+
+    u2 = R1(u1)
+    u3 = R1(u2)
+    
+    show_im(image(u1, X, Y), title="u1")
+    show_im(image(u2, X, Y), title="u2")
+    show_im(image(u3, X, Y), title="u3")
+    plt.show()
+
+    return u1, u2
+
+
+if __name__ == "__main__":
+    u1, u2 = main()
